d0d2715af8cd1c51d4064944b16422ce45d8721e
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-04-20 08:28:21 +0200

    Create chuck_noris.vue

---
+++ src/views/chuck_noris.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an norris page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import landing from '@/components/landing_page.vue'
+
+export default {
+  name: 'chuck',
+  components: {
+    chuck_noris_jokes
+  }
+}
+</script>
