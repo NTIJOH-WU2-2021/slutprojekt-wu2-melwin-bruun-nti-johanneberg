f45dea3b31c02ecf685f32a896b4997166a24e4a
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-04-20 08:28:19 +0200

    Create advice.vue

---
+++ src/views/advice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an Advice page</h1>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import landing from '@/components/landing_page.vue'
+
+export default {
+  name: 'Advice',
+  components: {
+    advice
+  }
+}
+</script>
