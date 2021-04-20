4be7983469750c49145e3f21bdd0d16be5b7c7a6
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-04-20 08:28:24 +0200

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import landing from '@/components/landing_page.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Home
+  }
+}
+</script>
