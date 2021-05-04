2f8a09f8ec32609593dc1e5e7e17ec1a75f14d3c
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-05-04 16:26:13 +0200

    Update landing_page.vue

--- src/components/landing_page.vue
+++ src/components/landing_page.vue
@@ -3,16 +3,19 @@
     <h2>Dette är en Chuck Noris Jokes API.</h2>
     <img alt="CHUCK bild" class="picture" src="../assets/chucknorris.png"/>
     <router-link to="/Chuck_Noris_Jokes">Chuck Noris Jokes</router-link> 
+    <p> Detta apiet ger dig chuck norris skämt efter varje knap tryckningen. Där du kan skratta med chuck han själv.</p>
   </div>
   <div class="box">
     <h2>Dette är en Trump Quotes API.</h2>
     <img alt="trump bild" class="picture1" src="../assets/trump.jpg"/>
     <router-link to="/TrumpQuotes">Trupms quotes</router-link> 
+    <p>Detta apiet ger dig Trump quotes. där du skriver in vilket nyckel ord du skall söka efter</p>
   </div>
   <div class="box">
     <h2>Dette är en Advice For you API.</h2>
     <img alt="advice bild" class="picture" src="../assets/advice.jpg"/>
     <router-link to="/Advice_For_you">Advice For you</router-link> 
+    <p>Du får en random advice.</p>
   </div>
 </template>
 
