3be5081c38ed23787078092b4d3382553d5d40c8
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-05-04 16:26:09 +0200

    Create jokesTemplate.vue

---
+++ src/components/jokesTemplate.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>Card of your jokes/advice/qoutes</h2>
+    <p>{{ card}}</p>
+
+
+    
+</template>
+
+
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Jokes',
+  components: {
+   
+  }
+}
+</script>
