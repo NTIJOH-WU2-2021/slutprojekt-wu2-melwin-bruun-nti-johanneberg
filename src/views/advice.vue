c68b9359078a2c792d0f58345aedb9c2a34e78ba
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2021-05-04 16:26:21 +0200

    Update advice.vue

--- src/views/advice.vue
+++ src/views/advice.vue
@@ -1,21 +1,45 @@
 <template>
 <div class="rubrik"> 
-  <h1>This is an Advice page</h1>
+  <h1>Random advice for you</h1>
 </div>
+<img alt="advice bild" class="picture" src="../assets/advice.jpg"/>
 <div class="main">
-  <AdviceForYou/>
+  <Jokes/>
 </div>
 </template>
 
 
 <script>
 // @ is an alias to /src
-import AdviceForYou from '@/components/advice_for_you.vue'
+/*to do anropa apiet vid sökning skicka som en prop till jokes*/
+/*gör created i dem andra fallen*/ 
+import Jokes from '@/components/jokesTemplate.vue'
 
 export default {
   name: ' advice_for_you',
   components: {
-    AdviceForYou
+    Jokes
   }
 }
 </script>
+
+<style scoped>
+
+.picture{
+  height: 20rem;
+  width: 30rem;
+}
+
+
+.main{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 30rem;
+  height: 30rem;
+  background-color: lightgrey
+
+}
+
+</style>
